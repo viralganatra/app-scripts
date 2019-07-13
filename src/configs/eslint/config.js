7cbadab7e7fa237cfb7d8b1d43d31ae84dd31745
Viral Ganatra <viral@viralganatra.com>
2019-07-13 18:12:29 +0100

    Upgrade ESLint config and upgrade ESLint version and supporting plugins

--- src/configs/eslint/config.js
+++ src/configs/eslint/config.js
@@ -2,25 +2,32 @@ const prettier = require('../prettier/config');
 
 module.exports = {
   'parser': 'babel-eslint',
-  'extends': ['airbnb', 'prettier', 'prettier/react'],
+  'extends': ['airbnb', 'prettier', 'prettier/react', 'plugin:jest/recommended'],
   'env': {
     'browser': true,
+    'node': true,
   },
   'rules': {
+    'consistent-return': ['error', { treatUndefinedAsUnspecified: true }],    'lines-between-class-members': ['error', 'always', { 'exceptAfterSingleLine': true }],
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'no-underscore-dangle': [
+      'error',
+      { allow: ['__REDUX_DEVTOOLS_EXTENSION__', '__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] },
+    ],
+    'no-unused-expressions': ['error', { allowShortCircuit: true }],
+
     'import/no-extraneous-dependencies': ['error', { 'devDependencies': true }],
     'import/prefer-default-export': ['off'],
 
-    'lines-between-class-members': ['error', 'always', { 'exceptAfterSingleLine': true }],
-    'no-unused-expressions': ['error', { allowShortCircuit: true }],
-
     'react/destructuring-assignment': ['off'],
+    'react/no-array-index-key': ['warn'],
     'react/no-did-update-set-state': ['warn'],
     'react/no-unused-prop-types': ['error', { 'skipShapeProps': true }],
     'react/require-default-props': ['warn'],
-    'react/no-array-index-key': ['warn'],
 
     'react/jsx-filename-extension': ['error', { 'extensions': ['.js', '.jsx'] }],
     'react/jsx-no-bind': ['error'],
+    'react/jsx-one-expression-per-line': ['off'],
 
     'react/sort-comp': ['error', {
       'order': [
@@ -35,6 +42,7 @@ module.exports = {
       ],
     }],
 
+    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
     'jsx-a11y/label-has-for': ['off'],
 
     'prettier/prettier': ['error', prettier],
@@ -59,7 +67,12 @@ module.exports = {
         'render': true,
       },
       'rules': {
-        'react/prop-types': ['off'],
+        'jest/expect-expect': ['error'],
+        'jest/lowercase-name': ['error'],
+        'jest/no-large-snapshots': ['error'],
+        'jest/no-test-return-statement': ['error'],
+        'jest/valid-describe': ['error'],
+        'jest/valid-expect-in-promise': ['error'],
       },
     },
   ],
