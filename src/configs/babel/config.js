1cc3b5c2326a7a258b66173ee64e7d5793f9b2d2
Viral Ganatra <viral@viralganatra.com>
2019-07-13 17:05:47 +0100

    Upgrade to Babel 7

--- src/configs/babel/config.js
+++ src/configs/babel/config.js
@@ -1,32 +1,38 @@
+const browserslist = require('browserslist');
+
+const { appDirectory } = require('../../utils')
+
 const IS_TEST_ENV = process.env.NODE_ENV === 'test';
 const IS_PROD_ENV = process.env.NODE_ENV === 'production';
-const IS_DEV = !IS_TEST_ENV && !IS_PROD_ENV;
+const IS_DEV_ENV = !IS_TEST_ENV && !IS_PROD_ENV;
+
+const targetBrowsers = browserslist.loadConfig({ path: appDirectory });
+const browsers = targetBrowsers ? { browsers: targetBrowsers } || { esmodules: true };
 
 const envTargets = IS_TEST_ENV
-  ? { node: '8.9' }
-  : { browsers: ['last 2 Chrome versions'] };
+  ? { node: 'current' }
+  : browsers;
 
-module.exports = {
+module.exports = () => ({
   presets: [
     [
-      require.resolve('babel-preset-env'),
+      require.resolve('@babel/preset-env'),
       {
-        modules: false,
         targets: envTargets,
-        debug: IS_DEV,
+        debug: IS_DEV_ENV,
       },
     ],
-    require.resolve('babel-preset-react'),
+    require.resolve('@babel/preset-react'),
   ],
   plugins: [
-    require.resolve('babel-plugin-transform-class-properties'),
-    require.resolve('babel-plugin-transform-object-rest-spread'),
+    require.resolve('@babel/plugin-proposal-class-properties'),
+    require.resolve('@babel/plugin-proposal-object-rest-spread'),
     IS_TEST_ENV
       ? null
       : require.resolve('babel-plugin-transform-react-remove-prop-types'),
 
     IS_TEST_ENV
-      ? require.resolve('babel-plugin-transform-es2015-modules-commonjs')
+      ? require.resolve('@babel/plugin-transform-modules-commonjs')
       : null,
   ].filter(Boolean),
-};
+});
--- src/utils.js
+++ src/utils.js
@@ -3,15 +3,27 @@ const path = require('path');
 const readPkgUp = require('read-pkg-up');
 const which = require('which');
 
-const { pkg, path: pkgPath } = readPkgUp.sync({
+const { package: pkg, path: pkgPath } = readPkgUp.sync({
   cwd: fs.realpathSync(process.cwd()),
 });
 
+const envIsSet = (name) => {
+  return (
+    process.env.hasOwnProperty(name) &&
+    process.env[name] &&
+    process.env[name] !== 'undefined'
+  )
+}
+
 const appDirectory = path.dirname(pkgPath);
+
+exports.appDirectory = appDirectory;
 
 exports.fromRoot = (...pathName) => path.join(appDirectory, ...pathName);
 
 exports.hasFile = (...p) => fs.existsSync(exports.fromRoot(...p));
+
+exports.pkg = pkg;
 
 exports.hasPkgProp = (prop) => Object.keys(pkg).includes(prop);
 
@@ -42,3 +54,52 @@ exports.resolveBin = (modName, { executable = modName, cwd = process.cwd() } = {
     throw error;
   }
 };
+
+exports.getConcurrentlyArgs = (scripts, { killOthers = true } = {}) => {
+  const colors = [
+    'bgBlue',
+    'bgGreen',
+    'bgMagenta',
+    'bgCyan',
+    'bgWhite',
+    'bgRed',
+    'bgBlack',
+    'bgYellow',
+  ];
+
+  scripts = Object.entries(scripts).reduce((all, [name, script]) => {
+    if (script) {
+      all[name] = script;
+    }
+
+    return all;
+  }, {});
+
+  const prefixColors = Object.keys(scripts)
+    .reduce(
+      (pColors, _s, i) =>
+        pColors.concat([`${colors[i % colors.length]}.bold.reset`]),
+      [],
+    )
+    .join(',');
+
+  return [
+    killOthers ? '--kill-others-on-fail' : null,
+    '--prefix', '[{name}]',
+    '--names', Object.keys(scripts).join(','),
+    '--prefix-colors', prefixColors,
+    ...Object.values(scripts).map(s => JSON.stringify(s)),
+  ].filter(Boolean);
+}
+
+exports.parseEnv = (name) => {
+  if (envIsSet(name)) {
+    try {
+      return JSON.parse(process.env[name]);
+    } catch (err) {
+      return process.env[name];
+    }
+  }
+
+  return false;
+}
